168e3ca1185d8eb55696e2a40fef1e5f9f55e5a5
drJAGartner <dr.j.a.gartner@gmail.com>
2016-09-22 11:21:52 -0400

    update product marking

--- virtual_marking_miner.py
+++ virtual_marking_miner.py
@@ -14,27 +14,60 @@ def url_to_soup(url):
     res = urllib2.build_opener(urllib2.HTTPCookieProcessor).open(req, timeout=4)
     return BeautifulSoup(res.read(), "html.parser")
 
-def main():
-    soup = url_to_soup('https://www.tivo.com/legal/patents')
+def get_dicts():
+    tivo_prods = url_to_soup('https://www.tivo.com/legal/patents')
 
     products = {}
     all_ids = set()
-    for p in soup.find_all('p'):
+    for p in tivo_prods.find_all('p'):
         if p.get_text().find('U.S. Pat. Nos.') != -1:
             txt = p.get_text()
             pivot = txt.find('U.S. Pat.')
             name = txt[:pivot-1]
             pivot_2 = txt[pivot+15:].find('.')
-            products[name] = map(lambda x: re.sub('[^a-zA-Z0-9]','',x, flags=re.UNICODE), txt[pivot+15:pivot+15+pivot_2].split(' '))
+            products[name] = filter(lambda x: len(x) > 3, map(lambda x: re.sub('[^a-zA-Z0-9]','',x, flags=re.UNICODE), txt[pivot+15:pivot+15+pivot_2].split(' ')))
 
     for k, v in products.iteritems():
         for pat in v:
             all_ids.add(pat)
 
-    print all_ids
+    patents = {}
+    for idd in list(all_ids):
+        print "Getting soup for ", idd
+        url = 'http://patents.com/us-' + idd + '.html'
+        pp = url_to_soup(url)
+        patents[idd] = {}
 
+        l_d = pp.find_all('div')
+        i = 0
+        for _d in l_d:
+            if i == 20:
+                ii = 0
+                for _ch in _d.findChildren():
+                    if ii == 50:
+                        iii = 0
+                        for _ch2 in _ch.findChildren():
+                            if iii==0:
+                                txt = re.sub('\n     ', ' ', _ch2.text)
+                                txt = re.sub('\n', ' ', txt)
+                                patents[idd]['Title'] = txt
+                            if iii==5:
+                                txt = re.sub('\n     ', ' ', _ch2.text)
+                                txt = re.sub('\n', ' ', txt)
+                                patents[idd]['Abstract'] = txt
+                            iii += 1
+                        break
+                    ii += 1
+                break
+            i += 1
 
+    return (products, patents)
 
 
 if __name__ == "__main__":
-    main()
+    (products, patents) = get_dicts()
+
+    for k, v in patents.iteritems():
+        print "Patent", k
+        for kk, vv in v.iteritems():
+            print kk, ":", vv
